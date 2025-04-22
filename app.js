be89c2f7b7b3ab8edfa95f5b0ffbef85a17b6adf
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2025-04-22 16:04:26 +0800

    style: pretter style

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import headerHTML from 'raw-loader!./header.html';
 let styleText = [0, 1, 2, 3].map((i) => require('raw-loader!./styles' + i + '.css').default);
 import preStyles from 'raw-loader!./prestyles.css';
 import replaceURLs from './lib/replaceURLs';
-import {default as writeChar, writeSimpleChar, handleChar} from './lib/writeChar';
+import { default as writeChar, writeSimpleChar, handleChar } from './lib/writeChar';
 import getPrefix from './lib/getPrefix';
 
 // Vars that will help us get er done
@@ -19,7 +19,7 @@ let animationSkipped = false, done = false, paused = false;
 let browserPrefix;
 
 // Wait for load to get started.
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   getBrowserPrefix();
   populateHeader();
   getEls();
@@ -39,7 +39,7 @@ async function startAnimation() {
     await writeTo(styleEl, styleText[3], 0, speed, true, 1);
   }
   // Flow control straight from the ghettos of Milwaukee
-  catch(e) {
+  catch (e) {
     if (e.message === "SKIP IT") {
       surprisinglyShortAttentionSpan();
     } else {
@@ -59,15 +59,15 @@ async function surprisinglyShortAttentionSpan() {
   style.textContent = "#work-text * { " + browserPrefix + "transition: none; }";
   style.textContent += txt;
   let styleHTML = "";
-  for(let i = 0; i < txt.length; i++) {
-     styleHTML = handleChar(styleHTML, txt[i]);
+  for (let i = 0; i < txt.length; i++) {
+    styleHTML = handleChar(styleHTML, txt[i]);
   }
   styleEl.innerHTML = styleHTML;
   createWorkBox();
 
   // There's a bit of a scroll problem with this thing
   let start = Date.now();
-  while(Date.now() - 1000 > start) {
+  while (Date.now() - 1000 > start) {
     workEl.scrollTop = Infinity;
     styleEl.scrollTop = pgpEl.scrollTop = Infinity;
     await Promise.delay(16);
@@ -83,7 +83,7 @@ let endOfSentence = /[\.\?\!]\s$/;
 let comma = /\D[\,]\s$/;
 let endOfBlock = /[^\/]\n\n$/;
 
-async function writeTo(el, message, index, interval, mirrorToStyle, charsPerInterval){
+async function writeTo(el, message, index, interval, mirrorToStyle, charsPerInterval) {
   if (animationSkipped) {
     // Lol who needs proper flow control
     throw new Error('SKIP IT');
@@ -112,7 +112,7 @@ async function writeTo(el, message, index, interval, mirrorToStyle, charsPerInte
 
     do {
       await Promise.delay(thisInterval);
-    } while(paused);
+    } while (paused);
 
     return writeTo(el, message, index, interval, mirrorToStyle, charsPerInterval);
   }
@@ -125,7 +125,7 @@ async function writeTo(el, message, index, interval, mirrorToStyle, charsPerInte
 function getBrowserPrefix() {
   // Ghetto per-browser prefixing
   browserPrefix = getPrefix(); // could be empty string, which is fine
-  styleText = styleText.map(function(text) {
+  styleText = styleText.map(function (text) {
     return text.replace(/-webkit-/g, browserPrefix);
   });
 }
@@ -161,23 +161,23 @@ function populateHeader() {
 //
 function createEventHandlers() {
   // Mirror user edits back to the style element.
-  styleEl.addEventListener('input', function() {
+  styleEl.addEventListener('input', function () {
     style.textContent = styleEl.textContent;
   });
 
   // Skip anim on click to skipAnimation
-  skipAnimationEl.addEventListener('click', function(e) {
+  skipAnimationEl.addEventListener('click', function (e) {
     e.preventDefault();
     animationSkipped = true;
   });
 
-  pauseEl.addEventListener('click', function(e) {
+  pauseEl.addEventListener('click', function (e) {
     e.preventDefault();
     if (paused) {
-      pauseEl.textContent = "Pause ||";
+      pauseEl.textContent = "Pause ⏸︎";
       paused = false;
     } else {
-      pauseEl.textContent = "Resume >>";
+      pauseEl.textContent = "Resume ⏵︎";
       paused = true;
     }
   });
@@ -189,14 +189,14 @@ function createEventHandlers() {
 function createWorkBox() {
   if (workEl.classList.contains('flipped')) return;
   workEl.innerHTML = '<div class="text">' + replaceURLs(workText) + '</div>' +
-                     '<div class="md">' + replaceURLs(md(workText)) + '<div>';
+    '<div class="md">' + replaceURLs(md(workText)) + '<div>';
 
   workEl.classList.add('flipped');
   workEl.scrollTop = 9999;
 
   // flippy floppy
   let flipping = 0;
-  require('mouse-wheel')(workEl, async function(dx, dy) {
+  require('mouse-wheel')(workEl, async function (dx, dy) {
     if (flipping) return;
     let flipped = workEl.classList.contains('flipped');
     let half = (workEl.scrollHeight - workEl.clientHeight) / 2;
